512e9bcc94839328053d0668c579deb1be11d8c6
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-14 07:07:11 +0900

    Create templates

---
+++ templates/main.cpp
@@ -0,0 +1,13 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print(T value) {
+  cout << value << endl;
+}
+
+int main() {
+  int N;
+  cin >> N;
+}
