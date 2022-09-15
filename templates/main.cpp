d91937483127d2bd9217ca2117baa7f1796f73f6
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-15 10:19:20 +0900

    Update templates

--- templates/main.cpp
+++ templates/main.cpp
@@ -2,9 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 template <typename T>
 void print(T value) {
   cout << value << endl;
+}
+
+template <typename T>
+map<T, int> Counter(vector<T> &vec) {
+  map<T, int> counter;
+  for (auto &&t : vec) counter[t]++;
+  return counter;
 }
 
 int main() {
--- templates/main.ts
+++ templates/main.ts
@@ -4,4 +4,11 @@ const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
 const input = () => inputLines[inputLinesIndex++];
 const print = (...messages: any) => console.log(...messages);
 
+const sum = (data: number[]) => data.reduce((a, b) => a + b);
+const Counter = <T>(arr: T[]) => {
+  const counter = new Map<T, number>();
+  for (const v of arr) counter.set(v, (counter.get(v) || 0) + 1);
+  return counter;
+};
+
 const N = +input();
