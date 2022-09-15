68c9727f54cbe466889daada36bf48401640d8cd
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-16 03:12:33 +0900

    Add cpp template: rep, sum

--- templates/main.cpp
+++ templates/main.cpp
@@ -2,11 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
 
 template <typename T>
 void print(T value) {
   cout << value << endl;
+}
+
+template <typename T>
+T sum(vector<T> &vec) {
+  return accumulate(vec.begin(), vec.end(), 0.0);
 }
 
 template <typename T>
