4dc7d2c0b5371e4312e3c6b55ef2d90b4e2bcb8a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-17 18:21:33 +0900

    AC: https://algo-method.com/tasks/1058 ts

---
+++ algo-method.com/tasks/1058/main.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+const print = (...messages: any) => console.log(...messages);
+
+const [A, M] = input().split(" ").map(Number);
+
+const ans = A | M;
+print(ans);
