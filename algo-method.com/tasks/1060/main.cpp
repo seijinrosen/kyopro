0fb09047aa76829dabe0bfab9f0b45a87d9fccb4
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-19 12:53:17 +0900

    AC: https://algo-method.com/tasks/1060 cpp

---
+++ algo-method.com/tasks/1060/main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print(T value) {
+  cout << value << endl;
+}
+
+int main() {
+  int A, M;
+  cin >> A >> M;
+
+  int ans = A ^ M;
+  print(ans);
+}
