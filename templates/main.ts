0f83791ba8b925a11c76920ea571a084bfb2e9da
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-20 17:40:25 +0900

    Add ts template: sorted()

--- templates/main.ts
+++ templates/main.ts
@@ -15,6 +15,10 @@ const Counter = <T>(arr: T[]) => {
 const range = (start: number, stop: number, step = 1) =>
   Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/BigInt#比較演算
+const sorted = <T extends number | bigint>(array: T[]) =>
+  array.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+
 const inputPairArray = (n: number) =>
   [...Array(n)].map(() => input().split(" ").map(Number) as [number, number]);
 
