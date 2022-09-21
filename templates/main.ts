2762bf5eb18e8d33a1db56e5f5ca66ebec922029
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-21 11:50:42 +0900

    Add ts template: yesNo()

--- templates/main.ts
+++ templates/main.ts
@@ -4,6 +4,7 @@ const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
 const input = () => inputLines[inputLinesIndex++];
 const print = (...messages: any) => console.log(...messages);
 
+const yesNo = (b: boolean) => (b ? "Yes" : "No");
 const sum = (data: number[]) => data.reduce((a, b) => a + b);
 const reverse = (s: string) => s.split("").reverse().join("");
 const Counter = <T>(arr: T[]) => {
