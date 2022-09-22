c8add606c82cd092a1ea579c29fcfaf34164927b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-22 19:52:45 +0900

    AC: https://algo-method.com/tasks/1067 ts

---
+++ algo-method.com/tasks/1067/main.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+const print = (...messages: any) => console.log(...messages);
+
+const N = +input();
+
+const arr = [
+  "1110111",
+  "0100100",
+  "1011101",
+  "1101101",
+  "0101110",
+  "1101011",
+  "1111011",
+  "0100111",
+  "1111111",
+  "1101111",
+];
+
+const ans = parseInt(arr[N], 2);
+print(ans);
