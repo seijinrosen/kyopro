647c0e8f0596c8e2bc7d5d863406dc09b4220da9
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-22 11:53:04 +0900

    Add python template: pairwise()

--- templates/main.py
+++ templates/main.py
@@ -1,3 +1,17 @@
+from itertools import tee
+from typing import Iterable, TypeVar
+
+_T = TypeVar("_T")
+
+
+def pairwise(iterable: Iterable[_T]) -> "zip[tuple[_T, _T]]":
+    # https://docs.python.org/ja/3/library/itertools.html#itertools.pairwise
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
 def yes_no(b: bool) -> str:
     return "Yes" if b else "No"
 
