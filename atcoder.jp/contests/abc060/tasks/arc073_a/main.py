842a490801df743d6de766e7973497940f52a15c
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-22 11:50:05 +0900

    Use pairwise()

--- atcoder.jp/contests/abc060/tasks/arc073_a/main.py
+++ atcoder.jp/contests/abc060/tasks/arc073_a/main.py
@@ -1,5 +1,19 @@
+from itertools import tee
+from typing import Iterable, TypeVar
+
+_T = TypeVar("_T")
+
+
+def pairwise(iterable: Iterable[_T]) -> "zip[tuple[_T, _T]]":
+    # https://docs.python.org/ja/3/library/itertools.html#itertools.pairwise
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
 N, T = map(int, input().split())
 T_LIST = list(map(int, input().split()))
 
-ans = sum(min(T, T_LIST[i + 1] - T_LIST[i]) for i in range(N - 1)) + T
+ans = sum(min(T, b - a) for a, b in pairwise(T_LIST)) + T
 print(ans)
