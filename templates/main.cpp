c2937570985ebe70f193ea3d58add4c70fb014b3
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-09-23 14:44:01 +0900

    関数の順番入れ替え

--- templates/main.cpp
+++ templates/main.cpp
@@ -19,9 +19,25 @@ void print(vector<T> &vec, string sep = " ") {
   cout << vec.back() << endl;
 }
 
-string yes_no(bool b) { return b ? "Yes" : "No"; }
+template <typename T>
+map<T, int> Counter(vector<T> &vec) {
+  map<T, int> counter;
+  for (auto &&t : vec) counter[t]++;
+  return counter;
+}
 
-string reverse(const string &s) { return string(s.rbegin(), s.rend()); }
+int bin2int(const string &bin) {
+  int ret = 0;
+  for (auto &&[i, c] : enumerate(reverse(bin)))
+    if (c == '1') ret += 1 << i;
+  return ret;
+}
+
+vector<pair<int, char>> enumerate(const string &s) {
+  vector<pair<int, char>> ret(s.size());
+  for (size_t i = 0; i < s.size(); i++) ret[i] = {i, s[i]};
+  return ret;
+}
 
 string my_slice(string &s, int start, int stop) {
   return s.substr(start, stop - start);
@@ -32,22 +48,18 @@ vector<T> my_slice(const vector<T> &vec, int start, int stop = -1) {
 }
 
 template <typename T>
+vector<pair<T, T>> pairwise(const vector<T> &vec) {
+  return zip(vec, my_slice(vec, 1));
+}
+
+string reverse(const string &s) { return string(s.rbegin(), s.rend()); }
+
+template <typename T>
 T sum(vector<T> &vec) {
   return accumulate(vec.begin(), vec.end(), 0.0);
 }
 
-vector<pair<int, char>> enumerate(const string &s) {
-  vector<pair<int, char>> ret(s.size());
-  for (size_t i = 0; i < s.size(); i++) ret[i] = {i, s[i]};
-  return ret;
-}
-
-int bin2int(const string &bin) {
-  int ret = 0;
-  for (auto &&[i, c] : enumerate(reverse(bin)))
-    if (c == '1') ret += 1 << i;
-  return ret;
-}
+string yes_no(bool b) { return b ? "Yes" : "No"; }
 
 template <typename T1, typename T2>
 vector<pair<T1, T2>> zip(const vector<T1> &a, const vector<T2> &b) {
@@ -55,18 +67,6 @@ vector<pair<T1, T2>> zip(const vector<T1> &a, const vector<T2> &b) {
   for (size_t i = 0; i < min(a.size(), b.size()); i++)
     ret.push_back({a[i], b[i]});
   return ret;
-}
-
-template <typename T>
-vector<pair<T, T>> pairwise(const vector<T> &vec) {
-  return zip(vec, my_slice(vec, 1));
-}
-
-template <typename T>
-map<T, int> Counter(vector<T> &vec) {
-  map<T, int> counter;
-  for (auto &&t : vec) counter[t]++;
-  return counter;
 }
 
 int main() {
