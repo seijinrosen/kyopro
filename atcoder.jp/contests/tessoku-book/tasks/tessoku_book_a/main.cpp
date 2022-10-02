3dea2f3499b65005945386728bc1528a0e60e593
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-03 04:57:13 +0900

    AC: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a cpp

---
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a/main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print(T value) {
+  cout << value << endl;
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  int ans = N * N;
+  print(ans);
+}
