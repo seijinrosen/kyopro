74ce759419ac63b930dd359c505546594471aa56
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-03 04:54:08 +0900

    AC: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a ts

---
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a/main.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+const print = (...messages: any) => console.log(...messages);
+
+const N = +input();
+
+const ans = N * N;
+print(ans);
