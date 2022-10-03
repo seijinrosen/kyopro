ecdbfb64419e9e8f1346a2eedd131fc5a2c5b82b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-04 08:16:02 +0900

    Update hs template

--- templates/main.hs
+++ templates/main.hs
@@ -2,12 +2,20 @@ main :: IO ()
 main = do
   n <- readLn
   [a, b] <- map read . words <$> getLine
+  [n, x] <- words <$> getLine
+  a <- words <$> getLine
   p <- map read . words <$> getLine
 
   let ans = n * n
   let ans = a + b
   print ans
+  let ans = x `elem` a
+  putStrLn $ yesNo ans
   putStrLn $ map toLowerAlphabet p
 
 toLowerAlphabet :: Int -> Char
 toLowerAlphabet x = ['a' .. 'z'] !! (x - 1)
+
+yesNo :: Bool -> String
+yesNo False = "No"
+yesNo True = "Yes"
