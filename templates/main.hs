0960e4c4309efefbf224f4a19d306676023ac1f2
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-04 07:43:04 +0900

    Update hs template

--- templates/main.hs
+++ templates/main.hs
@@ -1,9 +1,11 @@
 main :: IO ()
 main = do
   n <- readLn
+  [a, b] <- map read . words <$> getLine
   p <- map read . words <$> getLine
 
   let ans = n * n
+  let ans = a + b
   print ans
   putStrLn $ map toLowerAlphabet p
 
