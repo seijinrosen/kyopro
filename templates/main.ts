16d9df0e8cc50160d8c5cbd31c3cd137c44b7562
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-04 04:40:51 +0900

    Add ts template: asciiLowercase

--- templates/main.ts
+++ templates/main.ts
@@ -4,6 +4,8 @@ const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
 const input = () => inputLines[inputLinesIndex++];
 const print = (...messages: any) => console.log(...messages);
 
+const asciiLowercase = "abcdefghijklmnopqrstuvwxyz";
+
 const Counter = <T>(arr: T[]) => {
   const counter = new Map<T, number>();
   for (const v of arr) counter.set(v, (counter.get(v) || 0) + 1);
