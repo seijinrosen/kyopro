6f218f2b620e64576a6550ec12a5d720aba277ba
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-04 12:04:13 +0900

    Add hs template: itertoolsProduct

--- templates/main.hs
+++ templates/main.hs
@@ -2,10 +2,12 @@ main :: IO ()
 main = do
   n <- getInt
   [a, b] <- getIntList
+  ps <- getIntList
 
-  let ans = a + b
+  let ans = a `elem` ps
 
   print ans
+  putStrLn $ yesNo ans
 
 -- input functions
 getInt :: IO Int
@@ -17,6 +19,9 @@ getIntList = map read . words <$> getLine
 -- functions
 isDivisorOf100 :: Int -> Bool
 isDivisorOf100 x = 100 `mod` x == 0
+
+itertoolsProduct :: [a] -> [b] -> [(a, b)]
+itertoolsProduct ps qs = [(p, q) | p <- ps, q <- qs]
 
 toLowerAlphabet :: Int -> Char
 toLowerAlphabet x = ['a' .. 'z'] !! (x - 1)
