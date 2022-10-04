268d2b775ca00a337b02778064ee5b89c68c12a0
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-04 18:21:24 +0900

    AC: https://atcoder.jp/contests/abs/tasks/practice_1 cpp

---
+++ atcoder.jp/contests/abs/tasks/practice_1/main.cpp
@@ -0,0 +1,12 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B, C;
+  string S;
+  cin >> A >> B >> C >> S;
+
+  int ans = A + B + C;
+  cout << ans << " " << S << endl;
+}
