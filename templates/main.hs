988e431b944d032c6e4904dedc6889153f7e8e71
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-04 15:55:05 +0900

    Add hs template: combinations

--- templates/main.hs
+++ templates/main.hs
@@ -5,11 +5,11 @@ main = do
   ps <- getIntList
 
   let ans = a `elem` ps
-
   print ans
   putStrLn $ yesNo ans
 
 -- input functions
+
 getInt :: IO Int
 getInt = readLn
 
@@ -17,6 +17,13 @@ getIntList :: IO [Int]
 getIntList = map read . words <$> getLine
 
 -- functions
+
+-- https://wiki.haskell.jp/Old/sampou.org/Programming_玉手箱#組合せの生成
+combinations :: [a] -> Int -> [[a]]
+combinations _ 0 = [[]]
+combinations [] _ = []
+combinations (x : xs) n = map (x :) (combinations xs (n - 1)) ++ combinations xs n
+
 isDivisorOf100 :: Int -> Bool
 isDivisorOf100 x = 100 `mod` x == 0
 
