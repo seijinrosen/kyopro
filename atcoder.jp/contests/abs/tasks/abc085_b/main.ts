08b2837260e5894d31d8039ae1afdfe406338361
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-06 10:52:31 +0900

    AC: https://atcoder.jp/contests/abs/tasks/abc085_b ts

---
+++ atcoder.jp/contests/abs/tasks/abc085_b/main.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+const print = (...messages: any) => console.log(...messages);
+
+const range = (a: number, b?: number) =>
+  b ? [...Array(b - a)].map((_, i) => a + i) : [...Array(a)].map((_, i) => i);
+
+const main = () => {
+  const N = +input();
+  const D = range(N).map(() => +input());
+
+  const ans = new Set(D).size;
+
+  print(ans);
+};
+
+main();
