2b9a0d5f731e616dd8893bcc1aac07e24040aa4a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-08 00:08:37 +0900

    Add py template: input_int_pair_list()

--- templates/main.py
+++ templates/main.py
@@ -72,6 +72,10 @@ def input_int_list() -> "list[int]":
     return list(map(int, input().split()))
 
 
+def input_int_pair_list(n: int) -> "list[tuple[int, int]]":
+    return [tuple(map(int, input().split())) for _ in range(n)]
+
+
 def input_int_tuple_list(n: int) -> "list[tuple[int, int, int]]":
     return [tuple(map(int, input().split())) for _ in range(n)]
 
