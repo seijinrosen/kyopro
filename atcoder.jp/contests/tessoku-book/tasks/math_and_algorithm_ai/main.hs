31b07c822e8a6ba8b81ef3ff5b08b5d96f6d0cd9
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-08 13:19:17 +0900

    TLE: https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ai hs

---
+++ atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ai/main.hs
@@ -0,0 +1,39 @@
+import Control.Monad (replicateM)
+
+main :: IO ()
+main = do
+  [n, q] <- getIntList
+  a <- getIntList
+  lr <- getIntPairList q
+
+  let acc = 0 : accumulate a
+      ans = [acc !! r - acc !! (l -1) | (l, r) <- lr]
+
+  printVertically ans
+
+-- input functions
+
+getIntList :: IO [Int]
+getIntList = map read . words <$> getLine
+
+toPair :: [Int] -> (Int, Int)
+toPair [p, q] = (p, q)
+toPair _ = (0, 0)
+
+getIntPair :: IO (Int, Int)
+getIntPair = toPair . map read . words <$> getLine
+
+getIntPairList :: Int -> IO [(Int, Int)]
+getIntPairList n = replicateM n getIntPair
+
+-- print functions
+
+printVertically :: (Foldable t, Show a) => t a -> IO ()
+printVertically = mapM_ print
+
+-- functions
+
+accumulate :: [Int] -> [Int]
+accumulate [] = []
+accumulate [x] = [x]
+accumulate (x : y : xs) = x : accumulate (x + y : xs)
