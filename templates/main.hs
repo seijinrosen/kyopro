b88370c5eda30a60ad617b83fc1d2e379e8049ca
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-08 13:21:15 +0900

    Add hs template: getIntPairList, printVertically, accumulate

--- templates/main.hs
+++ templates/main.hs
@@ -27,7 +27,27 @@ getIntListList n = replicateM n getIntList
 getVerticalIntList :: Int -> IO [Int]
 getVerticalIntList n = replicateM n getInt
 
+toPair :: [Int] -> (Int, Int)
+toPair [p, q] = (p, q)
+toPair _ = (0, 0)
+
+getIntPair :: IO (Int, Int)
+getIntPair = toPair . map read . words <$> getLine
+
+getIntPairList :: Int -> IO [(Int, Int)]
+getIntPairList n = replicateM n getIntPair
+
+-- print functions
+
+printVertically :: (Foldable t, Show a) => t a -> IO ()
+printVertically = mapM_ print
+
 -- functions
+
+accumulate :: [Int] -> [Int]
+accumulate [] = []
+accumulate [x] = [x]
+accumulate (x : y : xs) = x : accumulate (x + y : xs)
 
 bin2int :: String -> Int
 bin2int b = sum [2 ^ i | (i, c) <- enumerate $ reverse b, c == '1']
