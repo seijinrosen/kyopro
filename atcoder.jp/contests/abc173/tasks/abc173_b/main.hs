483fa36db45e29afa0dffa50ccc8cd944211f8cd
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-10 22:47:52 +0900

    AC: https://atcoder.jp/contests/abc173/tasks/abc173_b hs

--- atcoder.jp/contests/abc173/tasks/abc173_b/main.hs
+++ atcoder.jp/contests/abc173/tasks/abc173_b/main.hs
@@ -1,13 +1,22 @@
 import Control.Monad (replicateM)
+import qualified Data.Map as Map
 
 main :: IO ()
 main = do
   n <- readLn
   s <- replicateM n getLine
 
-  let ans = [r ++ " x " ++ show (count r s) | r <- ["AC", "WA", "TLE", "RE"]]
+  let ans = solve s
 
   mapM_ putStrLn ans
 
-count :: Eq a => a -> [a] -> Int
-count x s = length $ filter (== x) s
+solve :: [String] -> [String]
+solve s = [r ++ " x " ++ maybe "0" show (Map.lookup r c) | r <- results]
+  where
+    c = counter s
+
+results :: [String]
+results = ["AC", "WA", "TLE", "RE"]
+
+counter :: (Ord k, Num a) => [k] -> Map.Map k a
+counter xs = Map.fromListWith (+) $ zip xs (repeat 1)
