75322203e5a282c71df77ccfa3aa048fb9a6167b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-11 01:53:51 +0900

    AC: https://atcoder.jp/contests/abc155/tasks/abc155_b cpp

---
+++ atcoder.jp/contests/abc155/tasks/abc155_b/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print(T value) {
+  cout << value << endl;
+}
+
+template <typename P, typename T>
+bool all(P pred, const vector<T> &vec) {
+  for (auto &&x : vec)
+    if (!pred(x)) return false;
+  return true;
+}
+
+bool even(int n) { return n % 2 == 0; }
+
+template <typename P, typename T>
+vector<T> filter(P pred, const vector<T> &vec) {
+  vector<T> ret;
+  for (auto &&x : vec)
+    if (pred(x)) ret.push_back(x);
+  return ret;
+}
+
+vector<int> input_vector(int n) {
+  vector<int> ret(n);
+  for (auto &&i : ret) cin >> i;
+  return ret;
+}
+
+int main() {
+  int N;
+  cin >> N;
+  auto A = input_vector(N);
+
+  auto ans =
+      all([](int a) { return a % 3 == 0 || a % 5 == 0; }, filter(even, A));
+
+  print(ans ? "APPROVED" : "DENIED");
+}
