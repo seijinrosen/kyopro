f500527d08fa3f6f225c799ee23359360ca4e26a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-11 23:37:11 +0900

    input_int_list() をラムダ式に変更

--- templates/main.py
+++ templates/main.py
@@ -97,10 +97,7 @@ def yes_no(b: Union[bool, int]) -> str:
 # input functions --------------------------------------------------------------
 Pair = Tuple[int, int]
 Triple = Tuple[int, int, int]
-
-
-def input_int_list() -> "list[int]":
-    return list(map(int, input().split()))
+input_int_list = lambda: list(map(int, input().split()))
 
 
 def input_int_pair_list(n: int) -> List[Pair]:
