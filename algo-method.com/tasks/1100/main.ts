c743cfe7e2aa9bffed6e7c9dcb46dd44b126d069
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-15 03:40:58 +0900

    AC: https://algo-method.com/tasks/1100 ts

---
+++ algo-method.com/tasks/1100/main.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+
+const sort = <T extends number | bigint>(array: T[], reverse = false) =>
+  reverse
+    ? array.slice().sort((a, b) => (a > b ? -1 : a < b ? 1 : 0))
+    : array.slice().sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+
+const main = () => {
+  const N = +input();
+  const A = input().split(" ").map(Number);
+
+  const ans = sort(A)[N - 2];
+  console.log(ans);
+};
+
+main();
