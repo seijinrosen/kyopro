47aca246549b94759f0e1d9a932f3a13d1124e12
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-16 05:27:56 +0900

    Use scanl1: https://algo-method.com/tasks/1101 hs

--- algo-method.com/tasks/1101/main.hs
+++ algo-method.com/tasks/1101/main.hs
@@ -1,12 +1,7 @@
 main :: IO ()
 main = do
-  n <- getLine
-  a <- map read . words <$> getLine
+  n <- readLn :: IO Int
+  a <- map read . words <$> getLine :: IO [Int]
 
-  let ans = accumulateMax a
+  let ans = scanl1 max a
   mapM_ print ans
-
-accumulateMax :: [Int] -> [Int]
-accumulateMax [] = []
-accumulateMax [x] = [x]
-accumulateMax (x : y : xs) = x : accumulateMax (max x y : xs)
