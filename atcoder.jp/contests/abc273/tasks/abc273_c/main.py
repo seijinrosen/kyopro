c845568dba8c672fd208a1e0dffc1664abbd4e53
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-16 03:29:25 +0900

    Simplify: https://atcoder.jp/contests/abc273/tasks/abc273_c pypy3

--- atcoder.jp/contests/abc273/tasks/abc273_c/main.py
+++ atcoder.jp/contests/abc273/tasks/abc273_c/main.py
@@ -1,12 +1,9 @@
-from bisect import bisect_right
 from collections import Counter
 
 N = int(input())
 A = list(map(int, input().split()))
 
-sorted_set_A = sorted(set(A))
-lst = [len(sorted_set_A) - bisect_right(sorted_set_A, a) for a in A]
-counter = Counter(lst)
+counter = sorted(Counter(A).items(), reverse=True)
+ans = [v for _, v in counter] + [0] * (N - len(counter))
 
-for k in range(N):
-    print(counter[k])
+print(*ans, sep="\n")
