3e46844af39f478aaf397317a8e85e45bfec470f
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-16 08:34:02 +0900

    Simplify: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_n pypy3

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_n/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_n/main.py
@@ -1,10 +1,4 @@
 from itertools import product
-from typing import Union
-
-
-def yes_no(b: Union[bool, int]) -> str:
-    return "Yes" if b else "No"
-
 
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
@@ -16,4 +10,4 @@ P = [a + b for a, b in product(A, B)]
 Q = {c + d for c, d in product(C, D)}
 
 ans = any(K - p in Q for p in P)
-print(yes_no(ans))
+print("Yes" if ans else "No")
