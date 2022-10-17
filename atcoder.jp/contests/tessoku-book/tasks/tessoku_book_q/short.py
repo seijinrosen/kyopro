2cac2776ba74557f806fb03f0f089d25101c025a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-18 06:53:44 +0900

    Shorten

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_q/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_q/short.py
@@ -5,8 +5,7 @@ d=[0,A[0]]
 for i in range(2,N):d.append(min(d[i-1]+A[i-1],d[i-2]+B[i-2]))
 r=[p:=N]
 while 1<p:
- if d[p-2]+A[p-2]==d[p-1]:p-=1
- else:p-=2
+ p-=(d[p-2]+A[p-2]!=d[p-1])+1
  r.append(p)
 print(len(r))
 print(*r[::-1])
