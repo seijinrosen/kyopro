64e4e3a625e0be5765ad12a10505ecd08b8e8ebe
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-18 07:10:42 +0900

    改行でなくスペースでも良いようだ

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_q/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_q/short.py
@@ -5,5 +5,4 @@ d=[0,A[0]]
 for i in range(2,N):d.append(min(d[i-1]+A[i-1],d[i-2]+B[i-2]))
 r=[p:=N]
 while 1<p:r.append(p:=p-(d[p-2]+A[p-2]!=d[p-1])-1)
-print(len(r))
-print(*r[::-1])
+print(len(r),*r[::-1])
