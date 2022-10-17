045bc7eed7a8ed8b9b770da38d6a75fe0c30fa86
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-18 07:13:09 +0900

    Shorten

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_q/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_q/short.py
@@ -2,7 +2,7 @@ N,*X=map(int,open(0).read().split())
 A=X[:N-1]
 B=X[N-1:]
 d=[0,A[0]]
-for i in range(2,N):d.append(min(d[i-1]+A[i-1],d[i-2]+B[i-2]))
+for i in range(1,N-1):d.append(min(d[i]+A[i],d[i-1]+B[i-1]))
 r=[p:=N]
 while 1<p:r.append(p:=p-(d[p-2]+A[p-2]!=d[p-1])-1)
 print(len(r),*r[::-1])
