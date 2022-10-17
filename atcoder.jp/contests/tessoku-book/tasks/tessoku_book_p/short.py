3c75b9ee127bcb6a77338439af0702a95c7a3188
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-18 02:32:27 +0900

    Shorten

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_p/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_p/short.py
@@ -1,3 +1,3 @@
-N=int(input());A=[*map(int,input().split())];B=[*map(int,input().split())];d=[0,A[0]]
+l=lambda:[*map(int,input().split())];N=int(input());A=l();B=l();d=[0,A[0]]
 for i in range(2,N):d.append(min(d[i-1]+A[i-1],d[i-2]+B[i-2]))
 print(d[-1])
