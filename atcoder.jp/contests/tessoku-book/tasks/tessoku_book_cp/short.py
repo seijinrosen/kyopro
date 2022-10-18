dd7299daad57640cfab44ac95888ca587a5f9d6b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-18 10:25:16 +0900

    abs() にエイリアスをつける

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cp/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cp/short.py
@@ -1,6 +1,7 @@
+a=abs
 N,*H=map(int,open(0).read().split())
-d=[0,abs(H[1]-H[0])]
-for i in range(2,N):d+=[min(d[i-1]+abs(H[i-1]-H[i]),d[i-2]+abs(H[i-2]-H[i]))]
+d=[0,a(H[1]-H[0])]
+for i in range(2,N):d+=[min(d[i-1]+a(H[i-1]-H[i]),d[i-2]+a(H[i-2]-H[i]))]
 r=[p:=N]
-while 1<p:r+=[p:=p-(d[p-2]+abs(H[p-2]-H[p-1])!=d[p-1])-1]
+while 1<p:r+=[p:=p-(d[p-2]+a(H[p-2]-H[p-1])!=d[p-1])-1]
 print(len(r),*r[::-1])
