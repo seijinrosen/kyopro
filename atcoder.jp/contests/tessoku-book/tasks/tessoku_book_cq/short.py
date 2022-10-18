0ad500991a3fc189ce4c58b6c76052370214b3fd
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-19 00:39:47 +0900

    Shorten

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cq/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cq/short.py
@@ -1,9 +1,8 @@
-N,S,*A=map(int,open(0).read().split())
-d=[[1]+[0]*S]
-for a in A:d+=[[a<=j and d[-1][j-a]or d[-1][j]for j in range(S+1)]]
-if d[N][S]<1:print(-1);exit()
-j=S
+N,s,*A=map(int,open(0).read().split())
+d=[[1]+[0]*s]
+for a in A:d+=[[a<=j and d[-1][j-a]or d[-1][j]for j in range(s+1)]]
+if d[N][s]<1:print(-1);exit()
 c=[]
 for i in range(N):
- if d[N-i-1][j]<1:c+=[N-i];j-=A[N-i-1]
+ if d[N-i-1][s]<1:c+=[N-i];s-=A[N-i-1]
 print(len(c),*c[::-1])
