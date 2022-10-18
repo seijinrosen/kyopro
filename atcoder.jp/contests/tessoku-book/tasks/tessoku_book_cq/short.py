fd672c769d3130ab540b180db6b9ba64cbab6a4a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-19 00:48:37 +0900

    Shorten

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cq/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cq/short.py
@@ -1,8 +1,9 @@
-N,s,*A=map(int,open(0).read().split())
+n,s,*A=map(int,open(0).read().split())
 d=[[1]+[0]*s]
 for a in A:d+=[[a<=j and d[-1][j-a]or d[-1][j]for j in range(s+1)]]
-if d[N][s]<1:print(-1);exit()
+if d[n][s]<1:print(-1);exit()
 c=[]
-for i in range(N):
- if d[N-i-1][s]<1:c+=[N-i];s-=A[N-i-1]
+while n:
+ if d[n-1][s]<1:c+=[n];s-=A[n-1]
+ n-=1
 print(len(c),*c[::-1])
