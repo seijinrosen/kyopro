9fa3b915caa1a88e21f91a9300033de287de5dd1
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-22 20:28:35 +0900

    Simplify

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_t/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_t/main.py
@@ -6,9 +6,9 @@ M = len(T)
 dp = [[0] * (M + 1)]
 
 for s in S:
-    row = [0] * (M + 1)
+    row = [0]
     for j, t in enumerate(T, start=1):
-        row[j] = max(dp[-1][j], row[j - 1], dp[-1][j - 1] + 1 if s == t else 0)
+        row += [max(dp[-1][j], row[-1], dp[-1][j - 1] + 1 if s == t else 0)]
     dp += [row]
 
 ans = dp[N][M]
