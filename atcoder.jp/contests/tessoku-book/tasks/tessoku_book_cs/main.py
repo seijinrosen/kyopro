707f19e47ff241fd08ae0a126d15d5247e003d3c
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-24 15:17:48 +0900

    Use pairwise

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cs/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_cs/main.py
@@ -1,3 +1,17 @@
+from itertools import tee
+from typing import Iterable, TypeVar
+
+_T = TypeVar("_T")
+
+
+def pairwise(iterable: Iterable[_T]) -> "zip[tuple[_T, _T]]":
+    # https://docs.python.org/ja/3/library/itertools.html#itertools.pairwise
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
 S = input()
 T = input()
 
@@ -7,8 +21,8 @@ dp = [range(M + 1)]
 
 for s in S:
     row = [dp[-1][0] + 1]
-    for j, t in enumerate(T, start=1):
-        row += [min(dp[-1][j] + 1, row[-1] + 1, dp[-1][j - 1] + (s != t))]
+    for (a, b), t in zip(pairwise(dp[-1]), T):
+        row += [min(b + 1, row[-1] + 1, a + (s != t))]
     dp += [row]
 
 ans = dp[N][M]
