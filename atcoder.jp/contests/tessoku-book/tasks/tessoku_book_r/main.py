2c4f8a05a038fefa913787234cbe1f8558ed46c5
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-24 16:24:22 +0900

    Simplify

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_r/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_r/main.py
@@ -3,7 +3,7 @@ A = list(map(int, input().split()))
 
 dp = [{0}]
 for a in A:
-    dp += [{j for j in range(S + 1) if j in dp[-1] or (a <= j and j - a in dp[-1])}]
+    dp += [{j for j in range(S + 1) if j in dp[-1] or j - a in dp[-1]}]
 
 ans = S in dp[N]
 print("Yes" if ans else "No")
