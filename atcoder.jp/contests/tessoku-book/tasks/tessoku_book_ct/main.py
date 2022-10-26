0b48bc7e3b76dda9ae4b1c04b44e4304cd1d3827
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-10-26 19:59:23 +0900

    Simplify

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ct/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ct/main.py
@@ -1,28 +1,11 @@
-from itertools import tee
-from typing import Iterable, TypeVar
-
-_T = TypeVar("_T")
-
-
-def pairwise(iterable: Iterable[_T]) -> "zip[tuple[_T, _T]]":
-    # https://docs.python.org/ja/3/library/itertools.html#itertools.pairwise
-    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
-
 N = int(input())
 S = input()
 
 # dp[l][r]: l 文字目から r 文字目までの部分における最長回文の長さ (0-indexed)
 dp = [[int(i == j) for j in range(N)] for i in range(N)]
 
-for i, (s, t) in enumerate(pairwise(S)):
-    dp[i][i + 1] = 1 + (s == t)
-
 # LEN = r - l
-for LEN in range(2, N):
+for LEN in range(1, N):
     for l in range(N - LEN):
         r = l + LEN
         dp[l][r] = max(
