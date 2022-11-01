6e297df56813b6582f6312209de0be3a9202173a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-02 00:02:42 +0900

    Simplify: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_x pypy3

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_x/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_x/main.py
@@ -4,16 +4,14 @@ from typing import List
 N = int(input())
 A = list(map(int, input().split()))
 
-ans = 0
 L: List[int] = []
 
 for a in A:
     pos = bisect_left(L, a)
-
-    if ans <= pos:
-        L.append(a)
-        ans += 1
-    else:
+    if pos < len(L):
         L[pos] = a
+    else:
+        L.append(a)
 
+ans = len(L)
 print(ans)
