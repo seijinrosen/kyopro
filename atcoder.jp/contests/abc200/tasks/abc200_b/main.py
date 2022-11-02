d19e4e5708ee2b3db067cc0dcf0895e1c1bb9e62
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-03 03:44:21 +0900

    Update: https://atcoder.jp/contests/abc200/tasks/abc200_b pypy3

--- atcoder.jp/contests/abc200/tasks/abc200_b/main.py
+++ atcoder.jp/contests/abc200/tasks/abc200_b/main.py
@@ -2,6 +2,6 @@ N, K = map(int, input().split())
 
 n = N
 for _ in range(K):
-    n = n // 200 if n % 200 == 0 else int(str(n) + "200")
+    n = n // 200 if n % 200 == 0 else 1000 * n + 200
 
 print(n)
