1f8980e3de1567edc522e9dc410bfd0bfeb6d6f9
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-03 01:20:38 +0900

    Update: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_x pypy3

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_x/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_x/main.py
@@ -1,17 +1,21 @@
 from bisect import bisect_left
-from typing import List
+from typing import Iterable, List
+
+
+def lis(xs: Iterable[int]) -> int:
+    """最長増加部分列 (Longest increasing subsequence)"""
+    L: List[int] = []
+    for x in xs:
+        pos = bisect_left(L, x)
+        if pos == len(L):
+            L.append(x)
+        else:
+            L[pos] = x
+    return len(L)
+
 
 N = int(input())
 A = list(map(int, input().split()))
 
-L: List[int] = []
-
-for a in A:
-    pos = bisect_left(L, a)
-    if pos < len(L):
-        L[pos] = a
-    else:
-        L.append(a)
-
-ans = len(L)
+ans = lis(A)
 print(ans)
