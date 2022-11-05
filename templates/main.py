f39ef25dced6f3d6c0837dd724321c1d251f868a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-05 13:08:00 +0900

    Add py template: eratosthenes()

--- templates/main.py
+++ templates/main.py
@@ -72,6 +72,21 @@ def div_ceil(a: int, b: int) -> int:
     return (a + b - 1) // b
 
 
+def eratosthenes(n: int) -> List[int]:
+    """N 以下の素数
+    >>> eratosthenes(20)
+    [2, 3, 5, 7, 11, 13, 17, 19]
+    """
+    deleted = [False] * (n + 1)
+    deleted[0] = deleted[1] = True
+    for i in range(int(n**0.5) + 1):
+        if deleted[i]:
+            continue
+        for j in range(i * 2, n + 1, i):
+            deleted[j] = True
+    return [i for i, b in enumerate(deleted) if not b]
+
+
 def even(n: int) -> bool:
     return n % 2 == 0
 
