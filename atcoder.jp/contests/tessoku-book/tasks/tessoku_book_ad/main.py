fde87ca01966b6b5abbdc968221f774924607151
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-07 10:42:33 +0900

    Update

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ad/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ad/main.py
@@ -1,13 +1,31 @@
+from functools import reduce
 from math import factorial
 
 
-def nCr(n: int, r: int) -> int:
+def nCr(n: int, r: int, mod: int = 0) -> int:
+    """組み合わせの数
+    >>> nCr(4, 2)
+    6
+    >>> MOD = 10**9 + 7
+    >>> nCr(4, 2, MOD)
+    6
+    >>> nCr(77777, 44444, MOD)
+    409085577
+    """
     if n < r:
         return 0
-    return factorial(n) // factorial(r) // factorial(n - r)
+    if mod == 0:
+        return factorial(n) // factorial(r) // factorial(n - r)
+
+    def factorial_mod(x: int) -> int:
+        return reduce(lambda a, i: a * i % mod, range(1, x + 1), 1)
+
+    a = factorial_mod(n)
+    b = factorial_mod(r) * factorial_mod(n - r)
+    return a * pow(b, mod - 2, mod) % mod
 
 
 N, R = map(int, input().split())
 
-ans = nCr(N, R) % 1000000007
+ans = nCr(N, R, 1000000007)
 print(ans)
