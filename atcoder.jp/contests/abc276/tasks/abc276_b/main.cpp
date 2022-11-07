d2ddd0dc713f634ec3e0c45bd5ee7bfba60f893e
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-08 03:35:14 +0900

    Use set: https://atcoder.jp/contests/abc276/tasks/abc276_b cpp

--- atcoder.jp/contests/abc276/tasks/abc276_b/main.cpp
+++ atcoder.jp/contests/abc276/tasks/abc276_b/main.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-vector<T> sort(const vector<T> &vec, bool reverse = false) {
-  vector<T> ret = {vec.begin(), vec.end()};
-  if (reverse)
-    sort(ret.begin(), ret.end(), greater());
-  else
-    sort(ret.begin(), ret.end());
-  return ret;
-}
-
 vector<pair<int, int>> input_pair_vector(int n) {
   vector<pair<int, int>> ret(n);
   for (auto &&[a, b] : ret) cin >> a >> b;
@@ -22,15 +12,15 @@ int main() {
   cin >> N >> M;
   auto AB = input_pair_vector(M);
 
-  vector<vector<int>> G(N);
+  vector<set<int>> G(N);
   for (auto &&[a, b] : AB) {
-    G[a - 1].push_back(b);
-    G[b - 1].push_back(a);
+    G[a - 1].insert(b);
+    G[b - 1].insert(a);
   }
 
   for (auto &&g : G) {
     cout << g.size();
-    for (auto &&x : sort(g)) cout << ' ' << x;
+    for (auto &&x : g) cout << ' ' << x;
     cout << endl;
   }
 }
