5f40389f175d9c1694317e5c36b34647370430da
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-07 10:34:56 +0900

    Update

--- templates/main.py
+++ templates/main.py
@@ -153,13 +153,22 @@ def nCr(n: int, r: int, mod: int = 0) -> int:
     """組み合わせの数
     >>> nCr(4, 2)
     6
+    >>> MOD = 10**9 + 7
+    >>> nCr(4, 2, MOD)
+    6
+    >>> nCr(77777, 44444, MOD)
+    409085577
     """
     if n < r:
         return 0
-    a = factorial(n)
-    b = factorial(r) * factorial(n - r)
     if mod == 0:
-        return a // b
+        return factorial(n) // factorial(r) // factorial(n - r)
+
+    def factorial_mod(x: int) -> int:
+        return reduce(lambda a, i: a * i % mod, range(1, x + 1), 1)
+
+    a = factorial_mod(n)
+    b = factorial_mod(r) * factorial_mod(n - r)
     return a * pow(b, mod - 2, mod) % mod
 
 
