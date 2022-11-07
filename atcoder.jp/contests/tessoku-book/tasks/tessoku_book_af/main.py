b8fcad5593d7f815ccee98506f24c462ac243fce
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-07 20:35:48 +0900

    Update: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_af pypy3

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_af/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_af/main.py
@@ -1,7 +1,7 @@
 N, A, B = map(int, input().split())
 
 dp = [False] * (N + 1)
-for i in range(A, N + 1):
-    dp[i] = not dp[i - A] or (B <= i and not dp[i - B])
+for i in range(N + 1):
+    dp[i] = any(not dp[i - x] for x in [A, B] if x <= i)
 
 print("First" if dp[N] else "Second")
