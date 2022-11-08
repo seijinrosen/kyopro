5a90e60458687a5ca6929cdd08ec2a20597d61d2
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-08 15:52:34 +0900

    Update: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_df main.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_df/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_df/main.py
@@ -1,12 +1,17 @@
 from functools import reduce
 from itertools import chain
 from operator import xor
-from typing import List, Tuple
+from typing import Iterable, List, Tuple
+
+
+def nim(iterable: Iterable[int]) -> bool:
+    return 0 < reduce(xor, iterable)
+
 
 N, H, W = map(int, input().split())
 AB: List[Tuple[int, int]] = [
     tuple(int(x) - 1 for x in input().split()) for _ in range(N)
 ]
 
-nim = reduce(xor, chain.from_iterable(AB))
-print("Second" if nim == 0 else "First")
+ans = nim(chain.from_iterable(AB))
+print("First" if ans else "Second")
