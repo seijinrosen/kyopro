121803e36b650fdf5887b52aa857e4d89951368e
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-08 16:00:00 +0900

    Update: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ah main.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ah/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ah/main.py
@@ -1,10 +1,14 @@
 from functools import reduce
 from operator import xor
-from typing import Set
+from typing import Iterable, Set
 
 
 def mex(st: Set[int]) -> int:
     return next(i for i in range(len(st) + 1) if i not in st)
+
+
+def nim(iterable: Iterable[int]) -> bool:
+    return 0 < reduce(xor, iterable)
 
 
 N, X, Y = map(int, input().split())
@@ -16,5 +20,5 @@ grundy = [0] * (MAX_A + 1)
 for i in range(X, MAX_A + 1):
     grundy[i] = mex({grundy[i - x] for x in [X, Y] if x <= i})
 
-nim = reduce(xor, (grundy[a] for a in A))
-print("Second" if nim == 0 else "First")
+ans = nim(grundy[a] for a in A)
+print("First" if ans else "Second")
