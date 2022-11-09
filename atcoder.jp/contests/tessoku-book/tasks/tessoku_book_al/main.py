1adce985a86d6fa38c7cea34eca3b2b96feb21c7
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-10 06:34:51 +0900

    Simplify: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_al main.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_al/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_al/main.py
@@ -6,8 +6,7 @@ LRH: List[Tuple[int, int, int]] = [tuple(map(int, input().split())) for _ in ran
 working_hours = [0] + [24] * D
 
 for l, r, h in LRH:
-    for day in range(l, r + 1):
-        working_hours[day] = min(working_hours[day], h)
+    working_hours[l : r + 1] = [min(working_hours[day], h) for day in range(l, r + 1)]
 
 ans = sum(working_hours)
 print(ans)
