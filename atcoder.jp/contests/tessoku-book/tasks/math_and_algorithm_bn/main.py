35690804c5be71bc8e86236be7851e0434660010
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-10 19:52:47 +0900

    AC: https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_bn main.py

---
+++ atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_bn/main.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple
+
+N = int(input())
+LR: List[Tuple[int, int]] = [tuple(map(int, input().split())) for _ in range(N)]
+
+now = 0
+ans = 0
+
+for l, r in sorted(LR, key=lambda t: t[1]):
+    if now <= l:
+        now = r
+        ans += 1
+
+print(ans)
