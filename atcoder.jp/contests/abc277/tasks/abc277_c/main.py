6102f20a2938cb479654bb23cfbec41ff4750a62
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-13 00:13:34 +0900

    Update: https://atcoder.jp/contests/abc277/tasks/abc277_c main.py

--- atcoder.jp/contests/abc277/tasks/abc277_c/main.py
+++ atcoder.jp/contests/abc277/tasks/abc277_c/main.py
@@ -1,23 +1,23 @@
 from collections import defaultdict, deque
-from typing import List, Tuple
+from typing import DefaultDict, List, Set, Tuple
 
 N = int(input())
 AB: List[Tuple[int, int]] = [tuple(map(int, input().split())) for _ in range(N)]
 
-dic: "defaultdict[int, set[int]]" = defaultdict(set)
+G: DefaultDict[int, Set[int]] = defaultdict(set)
 for a, b in AB:
-    dic[a].add(b)
-    dic[b].add(a)
+    G[a].add(b)
+    G[b].add(a)
 
 visited = {1}
 que = deque([1])
 
 while que:
-    nex = que.popleft()
-    for nn in dic[nex]:
-        if nn not in visited:
-            que.append(nn)
-            visited.add(nn)
+    v = que.popleft()
+    for nv in G[v]:
+        if nv not in visited:
+            visited.add(nv)
+            que.append(nv)
 
 ans = max(visited)
 print(ans)
