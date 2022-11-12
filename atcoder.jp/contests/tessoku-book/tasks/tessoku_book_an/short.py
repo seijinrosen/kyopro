17320209a63450f1f32d62c2ee545be0d7f1ee03
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-12 20:01:38 +0900

    Shorten: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_an short.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_an/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_an/short.py
@@ -1,2 +1,2 @@
-*A,=map(int,[*open(0)][1].split())
-print(sum(c*(c-1)*(c-2)//6for c in[A.count(i)for i in range(1,101)]))
+*A,=[*open(0)][1].split()
+print(sum(c*(c-1)*(c-2)//6for c in[A.count(str(i))for i in range(1,101)]))
