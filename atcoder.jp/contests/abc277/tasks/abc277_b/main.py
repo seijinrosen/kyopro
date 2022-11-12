103b48c8fe0a4a8fb2bf062c59b93d247b859d85
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-12 23:32:38 +0900

    Simplify: https://atcoder.jp/contests/abc277/tasks/abc277_b main.py

--- atcoder.jp/contests/abc277/tasks/abc277_b/main.py
+++ atcoder.jp/contests/abc277/tasks/abc277_b/main.py
@@ -2,13 +2,9 @@ N = int(input())
 S = [input() for _ in range(N)]
 
 
-def cond1(s: str) -> bool:
-    return s[0] in {"H", "D", "C", "S"}
+def pred(s: str) -> bool:
+    return s[0] in "HDCS" and s[1] in "A23456789TJQK"
 
 
-def cond2(s: str) -> bool:
-    return s[1] in {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}
-
-
-ans = all(map(cond1, S)) and all(map(cond2, S)) and len(set(S)) == N
+ans = all(map(pred, S)) and len(set(S)) == N
 print("Yes" if ans else "No")
