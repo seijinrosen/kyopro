e34f2442412b257608ea06cc1475b4843dedf9ab
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-13 02:59:57 +0900

    AC: https://atcoder.jp/contests/abc277/tasks/abc277_d main.py

---
+++ atcoder.jp/contests/abc277/tasks/abc277_d/main.py
@@ -0,0 +1,30 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+total = sum(A)
+ans = total
+
+top_card = 0
+current_sum = 0
+card_count = 0
+
+for a in sorted(A) * 2:
+    if a < top_card:
+        if top_card + 1 == M and a == 0:
+            card_count += 1
+        else:
+            break
+    elif a in [top_card, top_card + 1]:
+        current_sum += a
+        card_count += 1
+    else:
+        current_sum = a
+        card_count = 1
+
+    top_card = a
+    ans = min(ans, total - current_sum)
+
+    if card_count == N:
+        break
+
+print(ans)
