ca78ebf6e185b269f927f35cc59c63f06ff0c39c
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-15 15:55:02 +0900

    AC: https://atcoder.jp/contests/arc104/tasks/arc104_a main.cpp

---
+++ atcoder.jp/contests/arc104/tasks/arc104_a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int X = (A + B) / 2;
+  int Y = (A - B) / 2;
+
+  cout << X << ' ' << Y << endl;
+}
