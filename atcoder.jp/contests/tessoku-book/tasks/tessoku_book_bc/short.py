5d9615fdfaad0a3eaaca4d762860cff8cae4c812
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-17 04:16:31 +0900

    Shorten: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc short.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc/short.py
@@ -2,6 +2,6 @@ from bisect import*
 L=[]
 for r in[*open(0)][1:]:
  i=bisect_left(L,x:=int(r[2:]))
- if"2">r[0]:insort(L,x)
- elif"3">r[0]:L.pop(i)
+ if"2">r:insort(L,x)
+ elif"3">r:L.pop(i)
  else:print(L[i]if i<len(L)else-1)
