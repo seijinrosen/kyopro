0a726eddb37ec29d1ce1617677ed3e872fd9d416
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-16 23:03:14 +0900

    Shorten: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc short.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc/short.py
@@ -4,5 +4,5 @@ for _ in range(int(input())):
  q,x=map(int,input().split())
  i=bisect_left(l,x)
  if q<2:l.insert(i,x)
- if q==2:l.pop(i)
- if q>2:print(l[i]if i<len(l)else-1)
+ elif q<3:l.pop(i)
+ else:print(l[i]if i<len(l)else-1)
