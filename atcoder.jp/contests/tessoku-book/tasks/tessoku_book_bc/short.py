90bc8284ade27e4091e117ce95e130a5fbc74b65
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-16 23:30:11 +0900

    Shorten: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc short.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bc/short.py
@@ -1,8 +1,7 @@
 from bisect import*
 l=[]
 for r in[*open(0)][1:]:
- q,x=map(int,r.split())
- i=bisect_left(l,x)
- if q<2:l.insert(i,x)
- elif q<3:l.pop(i)
+ i=bisect_left(l,x:=int(r[2:]))
+ if r[0]<"2":l.insert(i,x)
+ elif r[0]<"3":l.pop(i)
  else:print(l[i]if i<len(l)else-1)
