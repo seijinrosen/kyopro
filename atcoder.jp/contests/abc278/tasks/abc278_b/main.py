f1f5842be190cddf2e7334a41d2b308d09b4065e
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-20 00:07:22 +0900

    Simplify: https://atcoder.jp/contests/abc278/tasks/abc278_b main.py

--- atcoder.jp/contests/abc278/tasks/abc278_b/main.py
+++ atcoder.jp/contests/abc278/tasks/abc278_b/main.py
@@ -1,51 +1,20 @@
-from typing import Tuple
-
 H, M = map(int, input().split())
 
 
-def to_str(i: int):
-    if i < 10:
-        return "0", str(i)
-    return str(i)
-
-
-def to_time(a: str, b: str, c: str, d: str) -> Tuple[int, int]:
-    h, m = int(a + b), int(c + d)
-    return h, m
-
-
-def to_another_time(a: str, b: str, c: str, d: str) -> Tuple[int, int]:
-    h, m = int(a + c), int(b + d)
-    return h, m
-
-
 def is_valid_time(h: int, m: int) -> bool:
-    return h < 24 and m < 60
+    a, b = divmod(h, 10)
+    c, d = divmod(m, 10)
+    return a * 10 + c < 24 and b * 10 + d < 60
 
 
-a, b = to_str(H)
-c, d = to_str(M)
+h, m = H, M
 
-while True:
-    another_time = to_another_time(a, b, c, d)
-    if is_valid_time(*another_time):
-        break
-
-    h, m = to_time(a, b, c, d)
-    if m <= 58:
-        m += 1
-        c, d = to_str(m)
-    elif m == 59 and h <= 22:
-        m = 0
+while not is_valid_time(h, m):
+    m += 1
+    if m == 60:
         h += 1
-        a, b = to_str(h)
-        c, d = to_str(m)
-    elif h == 23:
-        h = 0
         m = 0
-        a, b = to_str(h)
-        c, d = to_str(m)
+    if h == 24:
+        h = 0
 
-
-h, m = to_time(a, b, c, d)
 print(h, m)
