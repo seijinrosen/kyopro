abe644b3618e7da68c136c70be76caef8c325520
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-20 01:04:30 +0900

    Shorten: https://atcoder.jp/contests/abc278/tasks/abc278_c short.py

--- atcoder.jp/contests/abc278/tasks/abc278_c/short.py
+++ atcoder.jp/contests/abc278/tasks/abc278_c/short.py
@@ -1,6 +1,6 @@
 s={0}
 for r in[*open(0)][1:]:
- t,a,b=map(int,r.split())
- if t<2:s|={(a,b)}
- elif t<3:s-={(a,b)}
+ t,a,b=r.split()
+ if"2">t:s|={(a,b)}
+ elif"3">t:s-={(a,b)}
  else:print("No"if{(a,b),(b,a)}-s else"Yes")
