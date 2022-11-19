81e8863e7d4e468a5e7f4a18f556145f3471e781
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-20 00:11:00 +0900

    AC: https://atcoder.jp/contests/abc278/tasks/abc278_c main.py

---
+++ atcoder.jp/contests/abc278/tasks/abc278_c/main.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from typing import DefaultDict, List, Set, Tuple
+
+N, Q = map(int, input().split())
+TAB: List[Tuple[int, int, int]] = [tuple(map(int, input().split())) for _ in range(Q)]
+
+G: DefaultDict[int, Set[int]] = defaultdict(set)
+
+for t, a, b in TAB:
+    if t == 1:
+        G[a].add(b)
+
+    elif t == 2:
+        G[a].discard(b)
+
+    elif t == 3:
+        if b in G[a] and a in G[b]:
+            print("Yes")
+        else:
+            print("No")
