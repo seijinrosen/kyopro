be7d1c27d7d52bae6c0b0f08789ebe5cd32bba85
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-21 07:56:46 +0900

    Add py template: UnionFind

--- templates/main.py
+++ templates/main.py
@@ -14,6 +14,39 @@ _S = TypeVar("_S")
 _U = TypeVar("_U")
 
 
+class UnionFind:
+    # https://algo-method.com/descriptions/136
+    def __init__(self, n: int) -> None:
+        self.par = [-1] * n
+        self.rank = [0] * n
+        self.siz = [1] * n
+
+    def root(self, x: int) -> int:
+        if self.par[x] == -1:
+            return x
+        self.par[x] = self.root(self.par[x])
+        return self.par[x]
+
+    def is_same(self, x: int, y: int) -> bool:
+        return self.root(x) == self.root(y)
+
+    def unite(self, x: int, y: int) -> bool:
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:
+            return False
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.par[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        self.siz[rx] += self.siz[ry]
+        return True
+
+    def size(self, x: int) -> int:
+        return self.siz[self.root(x)]
+
+
 # more-itertools
 def iterate(func: Callable[[_T], _T], start: _T) -> Iterator[_T]:
     # https://more-itertools.readthedocs.io/en/stable/api.html#more_itertools.iterate
