f306fd12a0f94d345f129e52320d6aa222a38971
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-22 05:12:32 +0900

    Update bash template

--- templates/main.bash
+++ templates/main.bash
@@ -1,4 +1,14 @@
-read -r X
+read -r N
+read -ra A
 
-ans=$((10 - X / 200))
+ans=$((10 - N / 200))
 echo $ans
+
+ans=0
+for ((i = 0; i < N; i += 2)); do
+    if ((A[i] % 2 == 1)); then
+        ((ans++))
+    fi
+done
+
+echo "$ans"
