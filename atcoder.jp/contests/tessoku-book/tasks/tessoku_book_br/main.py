59ef6dfa9911715dc99c084260feaf40b35955c0
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-21 21:34:33 +0900

    AC: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_br main.py

---
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_br/main.py
@@ -0,0 +1,28 @@
+from collections import deque
+from functools import partial, reduce
+from operator import lshift, xor
+from typing import List, Tuple
+
+N, M = map(int, input().split())
+A = map(int, input().split())
+XYZ: List[Tuple[int, int, int]] = [
+    tuple(int(x) - 1 for x in input().split()) for _ in range(M)
+]
+
+start = sum((1 << i) * a for i, a in enumerate(A))
+goal = (1 << N) - 1
+
+dist = [-1] * (1 << N)
+dist[start] = 0
+
+que = deque([start])
+while que:
+    v = que.popleft()
+    for xyz in XYZ:
+        nv = reduce(xor, map(partial(lshift, 1), xyz), v)
+        if dist[nv] == -1:
+            dist[nv] = dist[v] + 1
+            que.append(nv)
+
+ans = dist[goal]
+print(ans)
