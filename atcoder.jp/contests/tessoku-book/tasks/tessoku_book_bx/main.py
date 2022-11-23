76a4f45e4508ad20293f2c872dca6b40b15c8776
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-24 02:30:53 +0900

    Simplify: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bx main.py

--- atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bx/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bx/main.py
@@ -1,20 +1,13 @@
-from bisect import bisect_left
+from bisect import bisect_left, bisect_right
 
 N, W, L, R = map(int, input().split())
 X = [0, *map(int, input().split()), W]
 
 MOD = 10**9 + 7
-dp = [1]
-dp_sum = [1]
+dp = [0, 1]
 
 for x in X[1:]:
-    pos_l = bisect_left(X, x - R) - 1
-    pos_r = bisect_left(X, x - L + 1) - 1
+    dp.append((dp[-1] + dp[bisect_right(X, x - L)] - dp[bisect_left(X, x - R)]) % MOD)
 
-    a = dp_sum[pos_r] if 0 <= pos_r else 0
-    b = dp_sum[pos_l] if 0 <= pos_l else 0
-    dp.append((a - b) % MOD)
-
-    dp_sum.append((dp_sum[-1] + dp[-1]) % MOD)
-
-print(dp[N + 1])
+ans = dp[-1] - dp[-2]
+print(ans % MOD)
