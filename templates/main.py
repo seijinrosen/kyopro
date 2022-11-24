ad8e1d7655cd9372af1571cb7104ca9e501f1820
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-24 11:46:47 +0900

    Add py template: divisors()

--- templates/main.py
+++ templates/main.py
@@ -134,6 +134,24 @@ def div_ceil(a: int, b: int) -> int:
     return (a + b - 1) // b
 
 
+def divisors(n: int) -> List[int]:
+    """約数列挙
+    >>> divisors(12)
+    [1, 2, 3, 4, 6, 12]
+    >>> divisors(827847039317)
+    [1, 909859, 909863, 827847039317]
+    """
+    res: list[int] = []
+    for i in range(1, n + 1):
+        if n < i * i:
+            break
+        if n % i == 0:
+            res.append(i)
+            if i * i != n:
+                res.append(n // i)
+    return sorted(res)
+
+
 def eratosthenes(n: int) -> List[int]:
     """N 以下の素数
     >>> eratosthenes(20)
