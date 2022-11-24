332a5a6607782a16dadaae8ebc5ca2c0fcde3dc9
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-24 10:11:17 +0900

    Shorten: https://atcoder.jp/contests/tessoku-book/tasks/typical90_a short.py

--- atcoder.jp/contests/tessoku-book/tasks/typical90_a/short.py
+++ atcoder.jp/contests/tessoku-book/tasks/typical90_a/short.py
@@ -2,10 +2,9 @@ N,L,K,*A=map(int,open(0).read().split())
 l=1
 r=L
 while 1<r-l:
- m=(l+r)//2;c=n=x=0
+ m=(l+r)//2;c=n=0
  for a in A:
-  if m<=a-n:c+=1;n=a
-  if c==K:x=m<=L-n;break
- if x:l=m
- else:r=m
+  if m<=a-n and m<=L-a:c+=1;n=a
+ if c<K:r=m
+ else:l=m
 print(l)
