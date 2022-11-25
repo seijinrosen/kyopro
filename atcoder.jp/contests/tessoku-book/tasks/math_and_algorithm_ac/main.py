39ddb313828870e13e4f56715b0b55601d7bc8d4
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-25 11:00:32 +0900

    Simplify: https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ac main.py

--- atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ac/main.py
+++ atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ac/main.py
@@ -1,11 +1,11 @@
 N, *A = map(int, open(0).read().split())
 
-dp1 = [0] * (N + 1)
-dp2 = [0] * (N + 1)
+dp1 = [0]
+dp2 = [0]
 
-for i, a in enumerate(A, 1):
-    dp1[i] = dp2[i - 1] + a
-    dp2[i] = max(dp1[i - 1], dp2[i - 1])
+for a in A:
+    dp1.append(dp2[-1] + a)
+    dp2.append(max(dp1[-2], dp2[-1]))
 
 ans = max(dp1[N], dp2[N])
 print(ans)
