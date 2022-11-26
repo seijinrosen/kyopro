fc0d1a4cc703259afa21bbd94e91364461e6d3ab
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-26 09:28:28 +0900

    Simplify: https://atcoder.jp/contests/dwacon6th-prelims/tasks/dwacon6th_prelims_a main.hs

--- atcoder.jp/contests/dwacon6th-prelims/tasks/dwacon6th_prelims_a/main.hs
+++ atcoder.jp/contests/dwacon6th-prelims/tasks/dwacon6th_prelims_a/main.hs
@@ -3,8 +3,10 @@ import Control.Monad (replicateM)
 main :: IO ()
 main = do
   n <- readLn
-  st <- replicateM n $ (\[p, q] -> (p, read q)) . words <$> getLine
+  st <- replicateM n $ do
+    [s, t] <- words <$> getLine
+    return (s, read t)
   x <- getLine
 
-  let ans = sum . map snd . tail $ dropWhile (\(s, t) -> s /= x) st
+  let ans = sum . map snd . tail $ dropWhile ((/= x) . fst) st
   print ans
