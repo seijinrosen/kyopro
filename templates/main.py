cc85f2c6e0814b66181b64849915cdd5bf989fea
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-28 04:24:54 +0900

    Add py template: int2base()

--- templates/main.py
+++ templates/main.py
@@ -199,6 +199,21 @@ def full_enumerate(lst: List[_T]) -> "Iterator[compress[_T]]":
         yield compress(lst, bits)
 
 
+def int2base(number: int, base: int) -> str:
+    d = {2: bin, 8: oct, 16: hex}
+    if base in d:
+        return d[base](number)[2:]
+    if number == 0:
+        return "0"
+
+    def f(n: int, b: int) -> str:
+        if n == 0:
+            return ""
+        return f(n // b, b) + str(n % b)
+
+    return f(number, base)
+
+
 def int2bin(number: int, width: int) -> str:
     return bin(number)[2:].zfill(width)
 
