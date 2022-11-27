7ff0c1cf6a5ee1a7ed227bbcc4fc3fca30bf4705
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-28 08:09:47 +0900

    Simplify: https://atcoder.jp/contests/ddcc2020-qual/tasks/ddcc2020_qual_a main.py

--- atcoder.jp/contests/ddcc2020-qual/tasks/ddcc2020_qual_a/main.py
+++ atcoder.jp/contests/ddcc2020-qual/tasks/ddcc2020_qual_a/main.py
@@ -1,9 +1,5 @@
-X, Y = map(int, input().split())
-
-
 def prize(n: int) -> int:
-    d = {3: 100000, 2: 200000, 1: 300000}
-    return d.get(n, 0)
+    return {3: 100000, 2: 200000, 1: 300000}.get(n, 0)
 
 
 def additional_prize(x: int, y: int) -> int:
@@ -11,6 +7,8 @@ def additional_prize(x: int, y: int) -> int:
         return 400000
     return 0
 
+
+X, Y = map(int, input().split())
 
 ans = prize(X) + prize(Y) + additional_prize(X, Y)
 print(ans)
