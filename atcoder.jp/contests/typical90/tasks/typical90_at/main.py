e64ff05db5d880252ba5c063c22ae10d4f70d702
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-11-29 16:18:56 +0900

    AC: https://atcoder.jp/contests/typical90/tasks/typical90_at main.py

--- atcoder.jp/contests/typical90/tasks/typical90_at/main.py
+++ atcoder.jp/contests/typical90/tasks/typical90_at/main.py
@@ -1,8 +1,7 @@
 from collections import Counter
 from itertools import product
 
-input()
-counters = (Counter(int(x) % 46 for x in input().split()).items() for _ in range(3))
+counters = (Counter(int(x) % 46 for x in row.split()).items() for row in [*open(0)][1:])
 
 ans = sum(
     x * y * z for (a, x), (b, y), (c, z) in product(*counters) if (a + b + c) % 46 == 0
