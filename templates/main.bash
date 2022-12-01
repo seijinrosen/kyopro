5f6b7093802036ed4dcf508ea0420639036235e7
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-01 09:48:41 +0900

    Update bash template

--- templates/main.bash
+++ templates/main.bash
@@ -31,5 +31,14 @@ if ((X == 1 && Y == 1)); then
 fi
 echo $ans
 
+ABCD=$(tr " " "\n" | sort -n)
+array=($ABCD)
+A=${array[0]}
+B=${array[1]}
+C=${array[2]}
+D=${array[3]}
+ans=$((A + B + C == D || A + D == B + C))
+((ans)) && echo "Yes" || echo "No"
+
 # tr
 # wc
