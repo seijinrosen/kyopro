5b0ebc67c0e221da8f000bf7ab5d9bb5cc24414a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-06 21:20:18 +0900

    Shorten: https://atcoder.jp/contests/typical90/tasks/typical90_r short.py

--- atcoder.jp/contests/typical90/tasks/typical90_r/short.py
+++ atcoder.jp/contests/typical90/tasks/typical90_r/short.py
@@ -1,4 +1,4 @@
 from math import*
 T,l,X,Y,Q,*E=map(int,open(0).read().split())
 l/=2
-for e in E:print(atan2(l-l*cos(a:=2*pi*e/T),dist((0,-l*sin(a)),(X,Y)))*180/pi)
+for e in E:print(atan2(l-l*cos(a:=tau*e/T),dist((0,-l*sin(a)),(X,Y)))*180/pi)
