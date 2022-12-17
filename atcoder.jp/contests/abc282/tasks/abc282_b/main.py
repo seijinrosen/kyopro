23b206097712da12d76e3387fa276704d34782d9
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-18 00:18:52 +0900

    AC: https://atcoder.jp/contests/abc282/tasks/abc282_b main.py

--- atcoder.jp/contests/abc282/tasks/abc282_b/main.py
+++ atcoder.jp/contests/abc282/tasks/abc282_b/main.py
@@ -3,5 +3,5 @@ from itertools import combinations
 N, M = map(int, input().split())
 S = [[c == "o" for c in input()] for _ in range(N)]
 
-ans = sum(all(x[j] or y[j] for j in range(M)) for x, y in combinations(S, 2))
+ans = sum(all(map(any, zip(*comb))) for comb in combinations(S, 2))
 print(ans)
