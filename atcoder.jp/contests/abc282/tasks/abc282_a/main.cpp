daa3c4ac091e805270f9e184574189ac6b5992de
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-18 01:18:03 +0900

    AC: https://atcoder.jp/contests/abc282/tasks/abc282_a main.cpp

---
+++ atcoder.jp/contests/abc282/tasks/abc282_a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int main() {
+  int K;
+  cin >> K;
+
+  cout << ascii_uppercase.substr(0, K) << endl;
+}
