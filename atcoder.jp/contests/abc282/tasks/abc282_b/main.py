8f0c4277d64b6a57d18545449397d68e3f60975d
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-17 23:19:08 +0900

    Simplify: https://atcoder.jp/contests/abc282/tasks/abc282_b main.py

--- atcoder.jp/contests/abc282/tasks/abc282_b/main.py
+++ atcoder.jp/contests/abc282/tasks/abc282_b/main.py
@@ -1,18 +1,7 @@
 from itertools import combinations
 
 N, M = map(int, input().split())
-S = [input() for _ in range(N)]
+S = [[c == "o" for c in input()] for _ in range(N)]
 
-cnt = 0
-
-for comb in combinations(range(N), 2):
-    a, b = comb
-    ans = True
-    for j in range(M):
-        if S[a][j] == "o" or S[b][j] == "o":
-            continue
-        ans = False
-    if ans:
-        cnt += 1
-
-print(cnt)
+ans = sum(all(x[j] or y[j] for j in range(M)) for x, y in combinations(S, 2))
+print(ans)
