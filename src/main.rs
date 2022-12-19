22eed1c998a11454003e9f60ae95d272e08937b6
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-19 16:25:26 +0900

    Rust 環境構築

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let n = read_ln();
+    let h = read_ln();
+    let w = read_ln();
+
+    let ans = (n - h + 1) * (n - w + 1);
+    println!("{}", ans);
+}
+
+fn read_ln() -> i32 {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+    let n: i32 = n.trim().parse().expect("Please type a number!");
+    n
 }
