72fdd6af97a77e24959884971cc0963713c8af6f
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-21 03:05:19 +0900

    Update rs template

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use std::io;
+use proconio::input;
+use std::cmp;
+// use std::io;
 
-const ASCII_UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const ASCII_UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 fn main() {
-    let k = read_ln();
+    // input
+    // let k = read_ln();
+    input! {
+        (a, b, c, d): (i64, i64, i64, i64),
+    }
 
-    let ans = &ASCII_UPPERCASE[..k];
-    println!("{}", ans);
+    // main
+    // let ans = &ASCII_UPPERCASE[..k];
+    let ans = cmp::max(a, c) <= cmp::min(b, d);
+
+    // output
+    println!("{}", if ans { "Yes" } else { "No" });
 }
 
-fn read_ln() -> usize {
-    let mut buf = String::new();
-    io::stdin()
-        .read_line(&mut buf)
-        .expect("Failed to read line");
-    buf.trim().parse().expect("Please type a number!")
-}
+// fn read_ln() -> usize {
+//     let mut buf = String::new();
+//     io::stdin()
+//         .read_line(&mut buf)
+//         .expect("Failed to read line");
+//     buf.trim().parse().expect("Please type a number!")
+// }
