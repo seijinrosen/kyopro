0780f8294760a73eec307f3132a1a26d7aec801a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-24 04:25:46 +0900

    Update zsh template: sum()

--- templates/main.zsh
+++ templates/main.zsh
@@ -1,1 +1,12 @@
+sum() {
+    local result=0
+    for x in $@; do
+        ((result += x))
+    done
+    echo $result
+}
 
+# https://atcoder.jp/contests/arc117/tasks/arc117_a
+s=$(sum $array)
+array+=$((-s))
+echo $array
