233f492f693866089e46d9be8139c4f0bcc97d6a
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-27 03:50:43 +0900

    AC: https://atcoder.jp/contests/arc012/tasks/arc012_1 main.bash

---
+++ atcoder.jp/contests/arc012/tasks/arc012_1/main.bash
@@ -0,0 +1,15 @@
+read -r day
+
+if [ "$day" == "Monday" ]; then
+    echo 5
+elif [ "$day" == "Tuesday" ]; then
+    echo 4
+elif [ "$day" == "Wednesday" ]; then
+    echo 3
+elif [ "$day" == "Thursday" ]; then
+    echo 2
+elif [ "$day" == "Friday" ]; then
+    echo 1
+else
+    echo 0
+fi
