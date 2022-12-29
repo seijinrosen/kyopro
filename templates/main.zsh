5cf5d6215cf1b6fda6cbcbdcacb93cc9405720af
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-29 09:23:30 +0900

    Add zsh template: rstrip()

--- templates/main.zsh
+++ templates/main.zsh
@@ -1,3 +1,15 @@
+rstrip() {
+    local s=$1
+    local c=$2
+    local n=${#s}
+    for ((i = n - 1; i >= 0; i--)); do
+        if ((${s:$i:1} != c)); then
+            break
+        fi
+        ((n--))
+    done
+    echo "${s:0:$n}"
+}
 sum() {
     local result=0
     for x in $@; do
