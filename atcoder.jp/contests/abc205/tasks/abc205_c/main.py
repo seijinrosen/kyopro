82e068b354d7808016c0491fde5efe08f56804fe
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-30 10:06:49 +0900

    Simplify: https://atcoder.jp/contests/abc205/tasks/abc205_c main.py

--- atcoder.jp/contests/abc205/tasks/abc205_c/main.py
+++ atcoder.jp/contests/abc205/tasks/abc205_c/main.py
@@ -2,12 +2,6 @@ A, B, C = map(int, input().split())
 
 
 def solve() -> str:
-    if 0 <= A and 0 <= B:
-        if A < B:
-            return "<"
-        if A > B:
-            return ">"
-        return "="
     if C % 2 == 0:
         if abs(A) < abs(B):
             return "<"
