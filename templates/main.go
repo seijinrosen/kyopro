802a2a8ccaa7aadd516747e67a56b9d230a9a37b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-04 10:56:24 +0900

    Update go template

--- templates/main.go
+++ templates/main.go
@@ -56,7 +56,18 @@ func main() {
 	var C int
 	fmt.Scanf("%g %g %d", &A, &B, &C)
 
+	// S := make([]string, N)
+	// for i := range S {
+	// 	fmt.Scanf("%s", &S[i])
+	// }
+
 	ans := IsPalindrome(strings.TrimRight(S, "0"))
+
+	// set := make(map[string]struct{})
+	// for _, v := range S {
+	// 	set[v] = struct{}{}
+	// }
+	// ans := len(set)
 
 	fmt.Println(ans)
 	fmt.Println(YesNo(ans))
