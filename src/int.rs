6cb2b9fbb3f6b8250ffba24529a50084767009ac
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-05 22:43:55 +0900

    Add rust tests

--- src/int.rs
+++ src/int.rs
@@ -3,7 +3,29 @@ pub trait MyInt {
 }
 
 impl MyInt for i32 {
+    /// 偶数かどうか。
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use kyopro::MyInt;
+    ///
+    /// assert_eq!(0.is_even(), true);
+    /// assert_eq!(1.is_even(), false);
+    /// ```
     fn is_even(&self) -> bool {
         self % 2 == 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_even() {
+        assert!(2.is_even());
+        assert!(!3.is_even());
+        assert!(4.is_even());
     }
 }
