ba3e3e399c9bb4cc99eb85ee5a678766cadd6d5f
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-05 17:52:25 +0900

    AC: https://atcoder.jp/contests/abc212/tasks/abc212_b main.bash

---
+++ atcoder.jp/contests/abc212/tasks/abc212_b/main.bash
@@ -0,0 +1,23 @@
+string_set_len() {
+    echo "$1" | fold -w1 | sort --unique | wc -l
+}
+
+is_weak1() {
+    echo $(($(string_set_len "$1") == 1))
+}
+
+is_weak2() {
+    local flag=1
+    for ((i = 0; i < 3; i++)); do
+        local x=${1:i:1}
+        local y=${1:i+1:1}
+        if (((x + 1) % 10 != y)); then
+            flag=0
+            break
+        fi
+    done
+    echo $flag
+}
+
+read -r XS
+(($(is_weak1 "$XS") || $(is_weak2 "$XS"))) && echo "Weak" || echo "Strong"
