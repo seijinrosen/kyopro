50175be34af8e69eb45040c7cf89d91c26c16e27
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-06 00:30:06 +0900

    AC: https://atcoder.jp/contests/code-festival-2015-quala/tasks/codefestival_2015_qualA_a main.ts

---
+++ atcoder.jp/contests/code-festival-2015-quala/tasks/codefestival_2015_qualA_a/main.ts
@@ -0,0 +1,6 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+
+console.log(input().slice(0, -1) + "5");
