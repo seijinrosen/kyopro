7560f858f68b9fe2b0ee217b0e73a49a683110bf
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-08 21:12:25 +0900

    Create script: prepare_atcoder.py

---
+++ scripts/prepare_atcoder.py
@@ -0,0 +1,23 @@
+import shutil
+from pathlib import Path
+from string import ascii_lowercase
+
+import pyperclip
+
+print("input contest url.")
+print("example: abc284")
+print()
+
+contest_name = input("type here: ")
+contest_dir_path = Path() / "atcoder.jp" / "contests" / contest_name / "tasks"
+templates_py_path = Path() / "templates" / "main.py"
+
+for ch in ascii_lowercase[:5]:
+    problem_dir_name = contest_name + "_" + ch
+    problem_dir_path = contest_dir_path / problem_dir_name
+    problem_dir_path.mkdir(parents=True, exist_ok=True)
+    shutil.copy(templates_py_path, problem_dir_path)
+
+problem_a = contest_name + "_a"
+to_copied = ["acac", "https://" + str(contest_dir_path / problem_a), "-m"]
+pyperclip.copy(" ".join(to_copied))  # type: ignore
