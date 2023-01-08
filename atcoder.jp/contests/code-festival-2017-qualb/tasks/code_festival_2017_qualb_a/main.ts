0b176e62e751f9a1770c6aba70a2a198183d2637
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-09 01:42:44 +0900

    AC: https://atcoder.jp/contests/code-festival-2017-qualb/tasks/code_festival_2017_qualb_a main.ts

---
+++ atcoder.jp/contests/code-festival-2017-qualb/tasks/code_festival_2017_qualb_a/main.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+
+const S = input();
+
+const ans = S.slice(0, S.length - 8);
+
+console.log(ans);
