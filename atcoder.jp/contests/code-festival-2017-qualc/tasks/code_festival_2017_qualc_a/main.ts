ce44dca5b1c46445315dd9fc097aec9aa053b95f
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-13 10:24:52 +0900

    AC: https://atcoder.jp/contests/code-festival-2017-qualc/tasks/code_festival_2017_qualc_a main.ts

---
+++ atcoder.jp/contests/code-festival-2017-qualc/tasks/code_festival_2017_qualc_a/main.ts
@@ -0,0 +1,6 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+
+console.log(input().includes("AC") ? "Yes" : "No");
