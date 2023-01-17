6290ad068919378e96d0ac8322e96bd51b4bc992
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-18 08:45:11 +0900

    AC: https://atcoder.jp/contests/abc166/tasks/abc166_b main.bash

---
+++ atcoder.jp/contests/abc166/tasks/abc166_b/main.bash
@@ -0,0 +1,16 @@
+read -r N K
+
+arr=()
+
+for ((i = 0; i < K; i++)); do
+    read -r _d
+    read -ra a
+
+    for v in "${a[@]}"; do
+        arr+=("$v")
+    done
+done
+
+st=($(echo "${arr[@]}" | tr " " "\n" | sort --unique))
+
+echo $((N - ${#st[@]}))
