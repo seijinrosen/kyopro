a7f4511ccd7916ab5c4932ff352cc6ee4b0b2c8b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-24 01:43:45 +0900

    AC: https://atcoder.jp/contests/abc172/tasks/abc172_a main.ts

---
+++ atcoder.jp/contests/abc172/tasks/abc172_a/main.ts
@@ -0,0 +1,8 @@
+import { readFileSync } from "fs";
+let inputLinesIndex = 0;
+const inputLines = readFileSync("/dev/stdin", "utf8").split("\n");
+const input = () => inputLines[inputLinesIndex++];
+
+const a = +input();
+
+console.log(a + a ** 2 + a ** 3);
