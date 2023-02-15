bfb9c80aa43eba8c01544b874ee9adca043a5b52
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-02-15 12:34:10 +0900

    AC: https://algo-method.com/tasks/1246 main.py

---
+++ algo-method.com/tasks/1246/main.py
@@ -0,0 +1,22 @@
+# 標準入力から値を受け取る
+# num: int 型
+num = int(input())
+
+# 受け取った値を利用してコードを書いてください
+alpha = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+]
+
+ans = alpha[num - 1]
+
+
+print(ans)
