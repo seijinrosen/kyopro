4dbf0a090853e80774afa148f683fe91b0d86768
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-07-08 20:56:54 +0900

    AC: https://algo-method.com/tasks/235 main.py

---
+++ algo-method.com/tasks/235/main.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def divisors(n: int) -> List[int]:
+    res: list[int] = []
+    for i in range(1, n + 1):
+        if n < i * i:
+            break
+        if n % i == 0:
+            res.append(i)
+            if i * i != n:
+                res.append(n // i)
+    return sorted(res)
+
+
+N, K = map(int, input().split())
+
+
+def f(n: int) -> bool:
+    return len(divisors(n)) == K
+
+
+def solve() -> int:
+    return sum(map(f, range(1, N + 1)))
+
+
+print(solve())
